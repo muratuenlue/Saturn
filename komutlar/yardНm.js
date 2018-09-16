1d53ce81b458e43167fb38dc76642421b524c0df
muratuenlue <muratuenlue@gmail.com>
2018-09-16 11:14:35 +0400

    Update yardНm.js

--- komutlar/yardНm.js
+++ komutlar/yardНm.js
@@ -11,8 +11,8 @@ exports.run = (client, message, params) => {
   .addField("**Eğlence ve Kullanıcı Komutları:**", `?banned = Dene ve Gör! \n?avatarım = Avatarınınızı Gösterir. \n?herkesebendençay = Herkese Çay Alırsınız. \n?koş = Koşarsınız.\n?çayiç = Çay İçersiniz. \n?çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n?çayaşekerat = Çaya Şeker Atarsınız. \n?yumruh-at = Yumruk Atarsınız. \n?yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \n?sunucuresmi = BOT Sunucunun Resmini Atar. \n?sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \n?kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
   .addField("**Yetkilisi Komutlar**", `?ban = İstediğiniz Kişiyi Sunucudan Banlar. \n?kick  = İstediğiniz Kişiyi Sunucudan Atar. \n?unban = İstediğiniz Kişinin Yasağını Açar. \n?sustur = İstediğiniz Kişiyi Susturur. \n?oylama = Oylama Açar. \n?duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
   .addField("**Ana Komutlar**", "?yardım = BOT Komutlarını Atar. \n?bilgi = BOT Kendisi Hakkında Bilgi Verir. \n?ping = BOT Gecikme Süresini Söyler. \n?davet = BOT Davet Linkini Atar. \n?istatistik = BOT İstatistiklerini Atar.")
-  .addField("**Yapımcı**", " **Kendiİsminiz** ")
-  .setFooter('**--------------------------**')
+  .addField("**Yapımcı**")
+  .setFooter('**@! RB P. |Dürüm Faruk!!#4104**')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
